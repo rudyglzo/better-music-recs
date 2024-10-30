b79d86c0ad4f60492884683b5aa5f8502a18bbc2
Rudy Garcilazo <rdf85@txstate.edu>
2024-10-30 13:38:09 -0500

    Frontend and Backend Configurations

--- frontend/spotion/src/app/app.component.spec.ts
+++ frontend/spotion/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'bmrfront' title`, () => {
+  it(`should have the 'spotion' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('bmrfront');
+    expect(app.title).toEqual('spotion');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, bmrfront');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, spotion');
   });
 });
